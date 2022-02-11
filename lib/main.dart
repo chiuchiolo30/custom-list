321ff714868207c1bbb8560bf71a2b2846b4583f
Edgardo <chiuchiolo30@gmail.com>
2022-02-10 22:04:29 -0300

    Utilizando gridview

--- lib/main.dart
+++ lib/main.dart
@@ -48,152 +48,109 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Padding(
         padding: const EdgeInsets.symmetric(vertical:8.0),
         child: SizedBox(
-          child: ListView.builder(
-            itemCount: 50,
-            itemBuilder:(context, index) {
-              int i = index + 1;
-              String image = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${(i*2)-1}.png';
-              String image2 = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${i*2}.png';
-              return FadeIn(
-                duration: const Duration( milliseconds: 500 ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: Padding(
-                        padding: const EdgeInsets.only(bottom:4.0),
-                        child: Card(
-                          child: Column(
-                            children: [
-                             Container(
-                               margin: const EdgeInsets.only(bottom: 4.0),
-                               color: const Color(0xfff2f2f2),                         
-                               child: Row(
-                                 mainAxisAlignment: MainAxisAlignment.center,
-                                 children:  [
-                                  FadeInImage(
-                                    placeholder: const AssetImage('assets/img/loading.gif'), 
-                                    image: NetworkImage(image),
-                                    width: 150,
-                                  )
-                                 ],
-                               ),
-                            
-                             ),
-                             Padding(
-                               padding: const EdgeInsets.only(left: 5.0, bottom:4.0),
-                               child: Column(
-                                 crossAxisAlignment: CrossAxisAlignment.start,
-                                 children: [
-                                    const Text('N.°001', style: TextStyle(color: Color(0xff919191), fontWeight: FontWeight.w600 )),
-                                    const SizedBox(height: 8.0,),
-                                    const Text('Bulbasaur', style: TextStyle(fontWeight: FontWeight.w600 )),
-                                    const SizedBox(height: 4.0,),
-                                    Row(
-                                      children:  [
-                                        DecoratedBox(
-                                          decoration: BoxDecoration(
-                                            color: const Color(0xff9bcc50),
-                                            borderRadius: BorderRadius.circular(4.0)
-                                          ),
-                                          child: const Padding(
-                                            padding: EdgeInsets.symmetric(vertical:2.0, horizontal: 16.0),
-                                            child: Text('Planta'),
-                                          )
-                                        ),
-                                        const SizedBox(width:4.0),
-                                        DecoratedBox(
-                                          decoration: BoxDecoration(
-                                            color: const Color(0xffb97fc9),
-                                            borderRadius: BorderRadius.circular(4.0)
-                                          ),
-                                          child: const Padding(
-                                            padding: EdgeInsets.symmetric(vertical:2.0, horizontal: 16.0),
-                                            child: Text('Veneno', style: TextStyle(color: Colors.white),),
-                                          )
-                                        ),
-                                      ],
-                                    )
-                                 ],
-                               ),
-                             )
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Padding(
-                        padding: const EdgeInsets.only(bottom:4.0),
-                        child: Card(
-                          child: Column(
-                            children: [
-                             Container(
-                               margin: const EdgeInsets.only(bottom: 4.0),
-                               color: const Color(0xfff2f2f2),
-                               child: Row(
-                                 mainAxisAlignment: MainAxisAlignment.center,
-                                 children:  [
-                                  FadeInImage(
-                                    placeholder: const AssetImage('assets/img/loading.gif'), 
-                                    image: NetworkImage(image2),
-                                    width: 150,
-                                  )
-                                 ],
-                               ),
-                           
-                             ),
-                             Padding(
-                               padding: const EdgeInsets.only(left: 5.0, bottom:4.0),
-                               child: Column(
-                                 crossAxisAlignment: CrossAxisAlignment.start,
-                                 children: [
-                                    const Text('N.°001', style: TextStyle(color: Color(0xff919191), fontWeight: FontWeight.w600 )),
-                                    const SizedBox(height: 8.0,),
-                                    const Text('Bulbasaur'),
-                                    const SizedBox(height: 4.0,),
-                                    Row(
-                                      children:  [
-                                        DecoratedBox(
-                                          decoration: BoxDecoration(
-                                            color: const Color(0xff9bcc50),
-                                            borderRadius: BorderRadius.circular(4.0)
-                                          ),
-                                          child: const Padding(
-                                            padding: EdgeInsets.symmetric(vertical:2.0, horizontal: 16.0),
-                                            child: Text('Planta'),
-                                          )
-                                        ),
-                                        const SizedBox(width:4.0),
-                                        DecoratedBox(
-                                          decoration: BoxDecoration(
-                                            color: const Color(0xffb97fc9),
-                                            borderRadius: BorderRadius.circular(4.0)
-                                          ),
-                                          child: const Padding(
-                                            padding: EdgeInsets.symmetric(vertical:2.0, horizontal: 16.0),
-                                            child: Text('Veneno', style: TextStyle(color: Colors.white),),
-                                          )
-                                        ),
-                                      ],
-                                    )
-                                 ],
-                               ),
-                             )
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  
-                  ],
-                ),
-              );
-            }, 
+          child: GridView.count(
+            crossAxisCount: 2,
+            children: List.generate(100, (index) => _CustomCard(index) ),
           ),
+        
         ),
+      ),
+    );
+  }
+}
+
+class _CustomCard extends StatelessWidget {
+  final int i;
+  const _CustomCard(this.i) : super();
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+               child: Column(
+                 children: [
+                  _Image(i: i),
+                  const _Information()
+                 ],
+               ),
+             );
+  }
+}
+
+class _Information extends StatelessWidget {
+  const _Information({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 5.0, bottom:4.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+             const Text('N.°001', style: TextStyle(color: Color(0xff919191), fontWeight: FontWeight.w600 )),
+             const SizedBox(height: 8.0,),
+             const Text('Bulbasaur'),
+             const SizedBox(height: 4.0,),
+             Row(
+               children:  [
+                 DecoratedBox(
+                   decoration: BoxDecoration(
+                     color: const Color(0xff9bcc50),
+                     borderRadius: BorderRadius.circular(4.0)
+                   ),
+                   child: const Padding(
+                     padding: EdgeInsets.symmetric(vertical:2.0, horizontal: 16.0),
+                     child: Text('Planta'),
+                   )
+                 ),
+                 const SizedBox(width:4.0),
+                 DecoratedBox(
+                   decoration: BoxDecoration(
+                     color: const Color(0xffb97fc9),
+                     borderRadius: BorderRadius.circular(4.0)
+                   ),
+                   child: const Padding(
+                     padding: EdgeInsets.symmetric(vertical:2.0, horizontal: 16.0),
+                     child: Text('Veneno', style: TextStyle(color: Colors.white),),
+                   )
+                 ),
+               ],
+             )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _Image extends StatelessWidget {
+  const _Image({
+    Key? key,
+    required this.i,
+  }) : super(key: key);
+
+  final int i;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 4.0),
+        color: const Color(0xfff2f2f2),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+           FadeInImage(
+             placeholder: const  AssetImage('assets/img/loading.gif'), 
+             image: NetworkImage('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${i + 1}.png'),                                    
+           )
+          ],
+        ),
+                               
       ),
     );
   }
